977f4b4d17899f6632605db1d78b5da6dc17dbbd
joenil <joenillam@gmail.com>
2015-11-11 03:06:52 +0800

    bug修复，支持打开txt

--- bin/kuhua.js
+++ bin/kuhua.js
@@ -10,16 +10,31 @@ var templateUrl = __dirname + '/../template/kuhua.html';
 var logFile = 'kuhua_url.txt';
 var epArr = function () {
 	var args = process.argv.slice(2) || [];
+	var pattern = /http:\/\/[a-z0-9\?\&\/\._=]+/img;
+	var ret = [];
 
-	args.forEach(function (item, index) {
-		var query = url.parse(item, true).query;
-		args[index] = {
-			comic_id: +query.comic_id,
-			ep_id: +query.ep_id
-		};
+	args.forEach(function (item) {
+		if (item.indexOf('.txt') >= 0) {
+			var txt = fs.readFileSync(item, 'utf-8');
+			var arr = txt.match(pattern) || [];
+
+			arr.forEach(function (item) {
+				push(item);
+			});
+		} else {
+			push(item);
+		}
 	});
 
-	return args;
+	function push(string) {
+		var query = url.parse(string, true).query;
+		ret.push({
+			comic_id: +query.comic_id,
+			ep_id: +query.ep_id
+		});
+	}
+
+	return ret;
 }();
 
 //开始生成
@@ -36,46 +51,36 @@ function generatePages(arr) {
 }
 
 function writeFile(ep, pageData) {
-	fs.readFile(templateUrl, function (err, data) {
-		if (err) {
-			console.log('读取模板文件失败');
-		} else {
-			console.log('开始生成文件...');
-			var template = data.toString('utf-8');
-			var content = ejs.render(template, pageData);
-			var filename = ['reader', ep.comic_id, ep.ep_id].join('-') + '.html';
+	var template = fs.readFileSync(templateUrl, 'utf-8');
+	var content = ejs.render(template, pageData);
+	var filename = ['reader', ep.comic_id, ep.ep_id].join('-') + '.html';
 
-			fs.writeFile(filename, content, function (err) {
-				if (err) {
-					console.log('创建失败！');
-				} else {
-					console.log('创建成功！');
-					createLog(filename, pageData);
-				}
-			});
-		}
-	});
+	console.log('开始生成文件...');
+	fs.writeFileSync(filename, content);
+	console.log('创建成功！');
+	createLog(filename, pageData);
 }
 
 function createLog(filename, pageData) {
 	var cur =  epArr.progress || 0;
 	var logText = '【' + pageData.comic_title + '】' + pageData.ep_title + '：\n' + getUrl(filename) + '\n';
+	var isExistLogFile = fs.existsSync(logFile);
 
 	epArr.progress = cur + 1;
 
-	if (epArr.progress === epArr.length) {
+	if (epArr.progress === 1) {
+		logText = '******** 生成时间：' + getTime() + ' ********\n' + logText;
+	} else if (epArr.progress === epArr.length) {
 		console.log('************');
 		console.log('共耗时：' + (Date.now() - beginTime) + 'ms');
-		logText += ('******** 生成时间：' + new Date().toLocaleString() + ' ********\n\n');
+		logText += ('\n\n');
 	}
 
-	fs.exists(logFile, function (exists) {
-		if (exists) {
-			fs.appendFile(logFile, logText);
-		} else {
-			fs.writeFile(logFile, logText);
-		}
-	});
+	if (isExistLogFile) {
+		fs.appendFile(logFile, logText);
+	} else {
+		fs.writeFileSync(logFile, logText);
+	}
 }
 
 //http://m.comicool.cn/act/201509/kuhua/reader-10090-4.html?ch=kh
@@ -86,4 +91,23 @@ function getUrl(filename) {
 	var yyyymm = year + (month > 9 ? '' + month : '0' + month);
 
 	return 'http://m.comicool.cn/act/' + yyyymm + '/kuhua/' + filename + '?ch=kh';
+}
+
+function getTime() {
+	var d = new Date();
+	var year = d.getFullYear();
+	var month = d.getMonth() + 1;
+	var date = d.getDate();
+	var day = d.getDay();
+	var hour = d.getHours();
+	var minute = d.getMinutes();
+	var second = d.getSeconds();
+	var fix = function (num) {
+		if (num < 10) {
+			return '0' + num;
+		}
+		return num;
+	};
+
+	return year + '年' + fix(month) + '月' + fix(date) + '日 ' + fix(hour) + ':' + fix(minute) + ':' + fix(second);
 }
